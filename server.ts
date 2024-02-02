8da96647407b561b06af2b75d83a5ce6f7943ec7
IgnaxA <ignat.panoff@gmail.com>
2024-02-03 00:19:44 +0500

    fixed ts annotation error

--- server.ts
+++ server.ts
@@ -4,6 +4,6 @@ const PORT = 8080;
 
 const app = express();
 
-app.listen(PORT, (err) => {
+app.listen(PORT, (err: Error) => {
     err ? console.log(err) : console.log(`Listening to port ${PORT}`);
 });

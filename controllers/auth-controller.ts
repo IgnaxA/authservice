8c6772d63c2605680ed4343a3269104c428d1e77
IgnaxA <ignat.panoff@gmail.com>
2024-02-11 18:57:56 +0500

    changing controllers for getting and sending needed data

--- controllers/auth-controller.ts
+++ controllers/auth-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
 export interface AuthController {
-    getUser(req: Request, res: Response): void;
-    createUser(req: Request, res: Response): void;
+    signIn(req: Request, res: Response): void;
+    signUp(req: Request, res: Response): void;
 };

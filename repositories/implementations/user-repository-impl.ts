779faff5037efdfd209120716717256c14295d5c
IgnaxA <ignat.panoff@gmail.com>
2024-02-11 19:00:47 +0500

    deleted old and adding needed requests to db

--- repositories/implementations/user-repository-impl.ts
+++ repositories/implementations/user-repository-impl.ts
@@ -13,28 +13,47 @@ export class UserRepositoryImpl implements UserRepository {
         user_id: JsonDecoder.number,
         user_email: JsonDecoder.string,
         user_password: JsonDecoder.string,
-        user_access_level: JsonDecoder.number
+        user_access_level: JsonDecoder.number,
+        user_refresh_token: JsonDecoder.string
     }, 'UserDB');
 
     private handlingQueryResult = async (queryResult: QueryResult<JSON>) => {
         const respond: User = await this.userDecoder.decodeToPromise(queryResult.rows[0])
-                                                    .then((userDB: UserDB) => new User(userDB.user_id, 
-                                                                                       userDB.user_email, 
-                                                                                       userDB.user_password, 
-                                                                                       userDB.user_access_level));
+                                                    .then((userDB: UserDB) => {
+                                                        const user: User = new User();
+                                                        user.setFullUser(userDB.user_id,
+                                                                         userDB.user_email, 
+                                                                         userDB.user_password, 
+                                                                         userDB.user_access_level,
+                                                                         userDB.user_refresh_token);
+                                                        return user;
+                                                    });
         return respond;
     };
 
-    public createUser = async (userEmail: string, userPassword: string, userAccessLevel: number): Promise<User> => {
-        return await pool.query(`INSERT INTO users (user_email, user_password, user_access_level) 
-                                 VALUES ('${userEmail}', '${userPassword}', ${userAccessLevel}) RETURNING *`)
+    public createUser = async (user: User): Promise<User> => {
+        return await pool.query(`INSERT INTO users (user_email, user_password, user_access_level, user_refresh_token) 
+                                 VALUES ('${user.getUserEmail()}', '${user.getUserPassword()}', ${user.getUserAccessLevel()}, '') 
+                                 RETURNING *`)
                           .then(async (queryResult: QueryResult<JSON>) => {
                             return this.handlingQueryResult(queryResult);
                           });
     };
- 
-    public getUser = async (userId: number): Promise<User> => {
-        return await pool.query(`SELECT * FROM users WHERE users.user_id = ${userId}`)
+
+    public getUser = async (userEmail: string): Promise<User> => {
+        return await pool.query(`SELECT * 
+                                 FROM users 
+                                 WHERE users.user_email = '${userEmail}'`)
+                         .then(async (queryResult: QueryResult<JSON>) => {
+                            return this.handlingQueryResult(queryResult);
+                         });
+    };
+
+    public setUserRefreshToken = async (user: User, userRefreshToken: string): Promise<User> => {
+        return await pool.query(`UPDATE users 
+                                 SET user_refresh_token = '${userRefreshToken}' 
+                                 WHERE user_id = ${user.getUserId()}
+                                 RETURNING *`)
                          .then(async (queryResult: QueryResult<JSON>) => {
                             return this.handlingQueryResult(queryResult);
                          });

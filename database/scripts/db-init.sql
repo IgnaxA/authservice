45bb1eb5cb81d4b8629a45ac6a1fde75c189c063
IgnaxA <ignat.panoff@gmail.com>
2024-02-11 18:54:46 +0500

    changing db attributes

--- database/scripts/db-init.sql
+++ database/scripts/db-init.sql
@@ -6,5 +6,6 @@ CREATE TABLE IF NOT EXISTS users (
     user_id serial PRIMARY KEY,
     user_email VARCHAR (40) UNIQUE NOT NULL,
     user_password VARCHAR (100) NOT NULL,
-    user_access_level SMALLINT NOT NULL
+    user_access_level SMALLINT NOT NULL,
+    user_refresh_token VARCHAR(200)
 );
